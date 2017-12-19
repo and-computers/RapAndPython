9af784df01b9b333536775a656ac3576b126c0d1
olutosinsonuyi <tosin.sonuyi@gmail.com>
2017-12-19 14:26:45 -0800

    adds filewriting to make text files of lyrics

--- webscrape/az_lyrics_scrape.py
+++ webscrape/az_lyrics_scrape.py
@@ -5,6 +5,8 @@ https://gist.github.com/managedkaos/e3262b80154129cc9a976ee6ee943da3
 
 # Requests is a library that allows you to programmatically send out http requests 
 import requests
+# os is a library for doing operating system things, like looking through file directories
+import os
 # BeautifulSoupp is a library made to allow developers to parse through the contents of a webpage
 from bs4 import BeautifulSoup
 url = "https://www.azlyrics.com/o/outkast.html"
@@ -31,7 +33,15 @@ for song_link in soup.find_all("a", href=True):
 	    print lyric_url
 	    response = requests.get(lyric_url, headers=headers)
 	    new_soup = BeautifulSoup(response.text,"lxml")
+	    filename = song_link.text.replace(' ','_').replace("'",'')
+	    filename += ".txt"
+	    filename = "scraped_data" + os.sep + filename
+	    print filename
+	    f = open(filename,"w+")
 	    for lyric in new_soup.find_all("div",{"class":None}):
 	    	print lyric.text
+	    	f = open(filename,"a")
+	    	f.write(lyric.text)
+	    f.close()
 
 
