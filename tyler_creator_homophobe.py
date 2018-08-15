106851c5914adae5d88b533977fc91b91103e744
tosin <tosin.sonuyi@gmail.com>
2018-08-15 19:10:39 -0400

    adds logger and some improved regexs
    regex kinda sucks

--- tyler_creator_homophobe.py
+++ tyler_creator_homophobe.py
@@ -2,6 +2,7 @@
 
 import os
 import re
+import logging
 
 import pandas as pd
 import numpy as np
@@ -9,23 +10,27 @@ import numpy as np
 import plotly.offline as pltly
 import plotly.graph_objs as go
 
+logger = logging.getLogger('rap_webscraper.{}'.format(__name__))
+
 songs_number_ref = {}
+songs_rape_reference = {}
 for root,dirs,fnameslist in os.walk("webscrape/scraped_data"):
 	if 'tyler_the_creator' in root:
 		for fname in fnameslist:
 			with open(os.path.join(root,fname)) as lyrics:
 				alltxt = lyrics.readlines()
-				found_homo = re.findall(r'fa[g]+', str(alltxt))
-				found_nig = re.findall(r'ga[y]+', str(alltxt))
-				found_bitch = re.findall(r'[b]+[i]+[t]+[c]+[h]+', str(alltxt))
-				# print fname
-				# print found_homo
-				# print found_nig
-				# print found_bitch
+				found_homo = re.findall(r'f[a]*g\w{0,}', str(alltxt))
+				found_nig = re.findall(r'g[a]*y\w{0,}', str(alltxt))
+				found_bitch = re.findall(r'b[i]*tch\w{0,}', str(alltxt))
+				rape_regex = r'rap[ie]\w{0,}'
+				found_rape = re.findall(rape_regex,str(alltxt))
 				num_homo = len(found_homo)
+				num_rape = len(found_rape)
 				songs_number_ref[fname] = (found_homo,num_homo)
+				songs_rape_reference[fname] = (found_rape,num_rape)
 
-print songs_number_ref
+logger.info(songs_number_ref)
+logger.info(songs_rape_reference)
 
 
 
