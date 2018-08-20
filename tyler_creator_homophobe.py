ff5007ef5349f1a79c109fae333672cc76ea0874
tosin <tosin.sonuyi@gmail.com>
2018-08-20 17:11:57 -0400

    adds logger, and makes references generic functions, adds bar graph plotting

--- tyler_creator_homophobe.py
+++ tyler_creator_homophobe.py
@@ -10,49 +10,103 @@ import numpy as np
 import plotly.offline as pltly
 import plotly.graph_objs as go
 
-logger = logging.getLogger('rap_webscraper.{}'.format(__name__))
-
-songs_number_ref = {}
-songs_rape_reference = {}
-for root,dirs,fnameslist in os.walk("webscrape/scraped_data"):
-	if 'tyler_the_creator' in root:
-		for fname in fnameslist:
-			with open(os.path.join(root,fname)) as lyrics:
-				alltxt = lyrics.readlines()
-				found_homo = re.findall(r'f[a]*g\w{0,}', str(alltxt))
-				found_nig = re.findall(r'g[a]*y\w{0,}', str(alltxt))
-				found_bitch = re.findall(r'b[i]*tch\w{0,}', str(alltxt))
-				rape_regex = r'rap[ie]\w{0,}'
-				found_rape = re.findall(rape_regex,str(alltxt))
-				num_homo = len(found_homo)
-				num_rape = len(found_rape)
-				songs_number_ref[fname] = (found_homo,num_homo)
-				songs_rape_reference[fname] = (found_rape,num_rape)
-
-logger.info(songs_number_ref)
-logger.info(songs_rape_reference)
+logger = logging.getLogger('lyrics_analysis.{}'.format(__name__))
+logger.setLevel(logging.DEBUG)
+# create file handler which logs even debug messages
+fh = logging.FileHandler('lyrics_analysis.log')
+fh.setLevel(logging.DEBUG)
+# create console handler with a higher log level
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+# create formatter and add it to the handlers
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+# add the handlers to the logger
+logger.addHandler(fh)
+logger.addHandler(ch)
 
 
+def make_reference_dict(artist_dir,regex):
+	"""
+	Create a dictionary of tuples specifying references in songs
+	takes in the directory containing the lyric files as well
+	as the regular expression to identify the references of interest
+	"""
+
+	songs_number_ref = {}
+	for root,dirs,fnameslist in os.walk("scraped_data"):
+		if 'tylerthecreator' in root:
+			for fname in fnameslist:
+				with open(os.path.join(root,fname)) as lyrics:
+					alltxt = lyrics.read()
+					found_ref = re.findall(regex,str(alltxt))
+					num_refs = len(found_ref)
+
+					yr = re.findall(r'\nALBUM INFO\r?\nalbum:\s"[\w{0,}\s{0,}]{0,}"\s\(\d+\)',alltxt)
+					try:
+						yr = int(re.findall(r'\d+',yr[0])[0])
+					except IndexError:
+						logger.warning('Was unable to find year in {}'.format(fname))
+						yr = np.nan
+					songs_number_ref[fname] = (found_ref,num_refs,yr)
+
+	return songs_number_ref
 
 
-traceFag = go.Scatter(x=df['PER'],y=df['Guaranteed'],mode='line',name='Fag',
-	text=df['Player'],
-	marker=dict(
-        size=16,
-        color = df['Win Percentage'], #set color equal to a variable
-        colorbar=dict(title='Team Win Percentage (%)'),
-        colorscale='Viridis',
-        showscale=True
-    ))
-traceFaggot = go.Scatter(x=df['BPM'],y=df['Guaranteed'],mode='line',name='Faggot',
-		text=df['Player'],
-	marker=dict(
-        size=16,
-        color = df['Win Percentage'], #set color equal to a variable
-        colorbar=dict(title='Team Win Percentage (%)'),
-        colorscale='Viridis',
-        showscale=True
-    ))
+def create_references_df(dictionary_of_3_tuples):
+	"""
+	Move from dictionary to pandas dataframe.
+
+	:param dictionary_of_3_tuples: dictionary of tuples, where the key 
+	is the name of the song file and the values is 3-tuple. The 3-tuple
+	has fields of (list of references found, num of references found, year of reference)
+
+	:returns: dataframe with the year, song name, number of references, and the words that match the reference
+	:rtype: pandas DataFrame
+	"""
+	df = pd.DataFrame()
+	# fname, num references, year
+	for songname, tuple_3 in dictionary_of_3_tuples.items():
+		songname = songname.replace('.txt','')
+		yr = tuple_3[2]
+		words_found = str(tuple_3[0]).replace('[','').replace(']','')
+		num_found = tuple_3[1]
+		newrow = pd.DataFrame([[songname,num_found,yr,words_found]],
+			columns=['song name','number of references','year','words'])
+		df = df.append(newrow,ignore_index=False)
+
+
+
+
+	return df
+
+#regex expressions
+homo_regex= r'f[a]*g\w{0,}'
+gay_regex = r'g[a]*y\w{0,}'
+bitch_regex = r'b[i]*tch\w{0,}'
+rape_regex = r'rap[ie]\w{0,}'
+
+songs_number_ref = make_reference_dict('tylerthecreator',regex=homo_regex)
+ref_df = create_references_df(songs_number_ref)
+logger.info(ref_df)
+
+grouped_ref_df = ref_df.groupby(['year'])
+counted_vals = grouped_ref_df.count()
+
+
+
+summed_ref_df = grouped_ref_df.sum()
+summed_ref_df['year'] = summed_ref_df.index.astype(int)
+
+
+
+
+
+traceHomophobe = go.Bar(x=summed_ref_df['year'],y=summed_ref_df['number of references'],
+	name='Homophobic References'
+	)
+
 
 
 layout = go.Layout(
@@ -66,7 +120,7 @@ layout = go.Layout(
         )
     ),
     yaxis=dict(
-        title='Homophobic (?) References',
+        title='Homophobic References',
         titlefont=dict(
             family='Courier New, monospace',
             size=18,
@@ -74,7 +128,7 @@ layout = go.Layout(
         )
     )
 )
-fig = go.Figure(data=[traceFag,traceFaggot], layout=layout)
+fig = go.Figure(data=[traceHomophobe], layout=layout)
 
 
 # data = [tracePER]
