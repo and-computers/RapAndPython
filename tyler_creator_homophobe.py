8617f27c4b450d45c840dd3fc2458a1e59b9b6df
tosin <tosin.sonuyi@gmail.com>
2018-08-20 17:41:13 -0400

    adds grouping into common function for references

--- tyler_creator_homophobe.py
+++ tyler_creator_homophobe.py
@@ -65,7 +65,7 @@ def create_references_df(dictionary_of_3_tuples):
 	:returns: dataframe with the year, song name, number of references, and the words that match the reference
 	:rtype: pandas DataFrame
 	"""
-	df = pd.DataFrame()
+	raw_df = pd.DataFrame()
 	# fname, num references, year
 	for songname, tuple_3 in dictionary_of_3_tuples.items():
 		songname = songname.replace('.txt','')
@@ -74,12 +74,22 @@ def create_references_df(dictionary_of_3_tuples):
 		num_found = tuple_3[1]
 		newrow = pd.DataFrame([[songname,num_found,yr,words_found]],
 			columns=['song name','number of references','year','words'])
-		df = df.append(newrow,ignore_index=False)
+		raw_df = raw_df.append(newrow,ignore_index=False)
 
 
+	grouped_df = raw_df.groupby('year')
+	summed_df = grouped_df.sum()
+	counted_df = grouped_df.count()
+
+	cumulative_df = summed_df
+
+	cumulative_df['year'] = summed_df.index.astype(int)
+	cumulative_df['number of references'] = summed_df['number of references']
+	cumulative_df['num songs'] = counted_df['song name']
+	cumulative_df['avg ref'] = cumulative_df['number of references']/cumulative_df['num songs']
 
 
-	return df
+	return raw_df,cumulative_df
 
 #regex expressions
 homo_regex= r'f[a]*g\w{0,}'
@@ -88,22 +98,16 @@ bitch_regex = r'b[i]*tch\w{0,}'
 rape_regex = r'rap[ie]\w{0,}'
 
 songs_number_ref = make_reference_dict('tylerthecreator',regex=homo_regex)
-ref_df = create_references_df(songs_number_ref)
-logger.info(ref_df)
-
-grouped_ref_df = ref_df.groupby(['year'])
-counted_vals = grouped_ref_df.count()
-
-
-
-summed_ref_df = grouped_ref_df.sum()
-summed_ref_df['year'] = summed_ref_df.index.astype(int)
+ref_df,grouped_df = create_references_df(songs_number_ref)
+logger.debug(ref_df)
+logger.info(grouped_df)
 
 
 
 
 
-traceHomophobe = go.Bar(x=summed_ref_df['year'],y=summed_ref_df['number of references'],
+
+traceHomophobe = go.Bar(x=grouped_df['year'],y=grouped_df['number of references'],
 	name='Homophobic References'
 	)
 
@@ -129,8 +133,4 @@ layout = go.Layout(
     )
 )
 fig = go.Figure(data=[traceHomophobe], layout=layout)
-
-
-# data = [tracePER]
-
 pltly.plot(fig)
