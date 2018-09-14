5822cae26d0e56a1924e7b456873665cc37b08ac
tosin <tosin.sonuyi@gmail.com>
2018-09-14 18:34:28 -0400

    artist mapping of datasuch as region and subregion'

---
+++ artist_data/artist_map.py
@@ -0,0 +1,13 @@
+# TODO create this programatically from csv if at all
+
+meta = {'2pac': 
+			{'region':'West',
+			 'subregion': 'LA'
+			}
+		'absoul':
+			{'region':'West'}
+			{'subregion': 'LA'}
+
+
+
+			}
