dd9ca8a8d34aefe0a3b5d56b6ec9d50403cbab6f
tosin <tosin.sonuyi@gmail.com>
2019-01-29 18:56:49 -0500

    take audio features from spotify and store in csv

--- spotify_api/spotipy_exploration.py
+++ spotify_api/spotipy_exploration.py
@@ -1,5 +1,8 @@
+import pdb
+import csv
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+# import pandas as pd
 
 with open('spotify_api/creds.txt') as f_h:
     creds = f_h.read()
@@ -9,9 +12,31 @@ sp = spotipy.Spotify(
 
 results = sp.search(q='soulja boy', limit=10)
 
+colnames = ['song_name', 'id', 'duration_ms', 'time_signature', 'danceability',
+            'energy', 'key', 'loudness', 'mode', 'speechiness', 'acousticness',
+            'release_date']
 
+
+songs_dicts = []
 for i, t in enumerate(results['tracks']['items']):
 
-    audio_analysis = sp.audio_analysis(t['id'])
-    audio_features = sp.audio_features(t['id'])
+    track_object = sp.track(t['id'])
+    release_date = track_object['album']['release_date']
+    # returns a list of one element
+    audio_features = sp.audio_features(t['id'])[0]
     print(' ', i, t['name'], t['id'], audio_features)
+
+    rowdict = {'release_date': release_date, 'song_name': t['name']}
+    for col in colnames:
+        try:
+            rowdict[col] = audio_features[col]
+        except KeyError as ke:
+            continue
+
+    songs_dicts.append(rowdict)
+
+with open('spotify_api/audio_data/soulja_boy.csv', 'w+') as f_h:
+    datawriter = csv.DictWriter(f_h, fieldnames=colnames)
+    datawriter.writeheader()
+    for x in songs_dicts:
+        datawriter.writerow(x)
