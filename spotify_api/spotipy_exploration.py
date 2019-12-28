c0a35adb14855601e1da033935b73700bd1f6014
tosin <tosin.sonuyi@gmail.com>
2019-12-27 23:24:20 -0800

    [wip] refactor auth-flow & add func for playlist reading

--- spotify_api/spotipy_exploration.py
+++ spotify_api/spotipy_exploration.py
@@ -4,39 +4,105 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 # import pandas as pd
 
-with open('spotify_api/creds.txt') as f_h:
-    creds = f_h.read()
 
-sp = spotipy.Spotify(
-    auth=creds)
-
-results = sp.search(q='soulja boy', limit=50)
-
-colnames = ['song_name', 'id', 'duration_ms', 'time_signature', 'danceability',
-            'energy', 'key', 'loudness', 'mode', 'speechiness', 'acousticness',
-            'release_date']
+AUDIO_FEATURE_COLS = ['song_name', 'id', 'duration_ms', 'time_signature', 'danceability',
+                      'energy', 'key', 'loudness', 'mode', 'speechiness', 'acousticness',
+                      'release_date']
 
 
-songs_dicts = []
-for i, t in enumerate(results['tracks']['items']):
+def get_client_id(fname='spotify_api/creds/client_id.txt'):
+    """
+    get spotify credentials for api access
+    """
 
-    track_object = sp.track(t['id'])
-    release_date = track_object['album']['release_date']
-    # returns a list of one element
-    audio_features = sp.audio_features(t['id'])[0]
-    print(' ', i, t['name'], t['id'], audio_features)
+    with open(fname) as f_h:
+        creds = f_h.read()
 
-    rowdict = {'release_date': release_date, 'song_name': t['name']}
-    for col in colnames:
-        try:
-            rowdict[col] = audio_features[col]
-        except KeyError as ke:
-            continue
+    return creds
 
-    songs_dicts.append(rowdict)
 
-with open('spotify_api/audio_data/soulja_boy.csv', 'w+') as f_h:
-    datawriter = csv.DictWriter(f_h, fieldnames=colnames)
-    datawriter.writeheader()
-    for x in songs_dicts:
-        datawriter.writerow(x)
+def get_client_secret(fname='spotify_api/creds/client_secret.txt'):
+    """
+    get client secret
+    """
+    with open(fname) as f_h:
+        creds = f_h.read()
+
+    return creds
+
+
+def generate_token():
+    """
+    Generate the token.
+    """
+    client_id = get_client_id()
+    client_secret = get_client_secret()
+    credentials = SpotifyClientCredentials(
+        client_id=client_id,
+        client_secret=client_secret)
+    token = credentials.get_access_token()
+    return token
+
+
+def read_playlist(spotify, username, playlist_id, fields='tracks,next,name'):
+    """
+    get the tracks from a playlist
+    """
+    playlist_results = spotify.user_playlist(username, playlist_id,
+                                             fields=fields)
+    # text_file = u'{0}.txt'.format(results['name'], ok='-_()[]{}')
+    num_tracks = playlist_results['tracks']['total']
+    print(f'Read {num_tracks} tracks')
+    playlist_tracks = playlist_results['tracks']['items']
+    # write_tracks(text_file, tracks)
+    return playlist_tracks
+
+
+def write_data(spotify, track_results, fname='spotify_api/audio_data/yada.csv'):
+    """
+    write data to a file
+    """
+
+    songs_dicts = []
+    for i, t in enumerate(track_results['tracks']['items']):
+
+        track_object = spotify.track(t['id'])
+        release_date = track_object['album']['release_date']
+        # returns a list of one element
+        audio_features = spotify.audio_features(t['id'])[0]
+        print(' ', i, t['name'], t['id'], audio_features)
+
+        rowdict = {'release_date': release_date, 'song_name': t['name']}
+        for col in AUDIO_FEATURE_COLS:
+            try:
+                rowdict[col] = audio_features[col]
+            except KeyError as ke:
+                continue
+
+        songs_dicts.append(rowdict)
+
+    with open(fname, 'w+') as f_h:
+        datawriter = csv.DictWriter(f_h, fieldnames=AUDIO_FEATURE_COLS)
+        datawriter.writeheader()
+        for x in songs_dicts:
+            datawriter.writerow(x)
+
+
+def main():
+    """
+    run the functions
+    """
+    creds = generate_token()
+    SP = spotipy.Spotify(auth=creds)
+
+    PLAYLIST_ID = '0eCUnMZEBIlbR5tHTdnJN3'
+    USERNAME = 'omo_desol'
+
+    playlist_information = read_playlist(spotify=SP, username=USERNAME, playlist_id=PLAYLIST_ID)
+    playlist_information['items'][0]['track']['id']
+    tracks, artists = create_spotify_objects(playlist_information)
+    import pdb
+    pdb.set_trace()
+
+if __name__ == "__main__":
+    main()
